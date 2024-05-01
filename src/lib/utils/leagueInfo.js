27ccdcc4f5795235cd5a613b7afcebea601fa4de
johnnyrockerrr <152285504+johnnyrockerrr@users.noreply.github.com>
2024-05-01 12:52:05 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "918226881823039488"; // your league ID
+export const leagueID = "1048184656186806272"; // your league ID
 export const leagueName = "Transatlantic Super League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
